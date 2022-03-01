83ff704c6bafa6b33086f2fe5d9fb4067d4b131a
mahmoud osama <mahmoud.osaamaa@gmail.com>
2022-03-01 16:37:53 +0200

    Removing the onResult method as deprecated
        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
            super.onActivityResult(requestCode, resultCode, data)
    
            if (requestCode == REQUEST_CHECK_SETTINGS) //if the result code related to the resultcode i'm using for GPS Enabling that i defined as
            //    public static final int REQUEST_CHECK_SETTINGS=12; //any value i want
            {
                when (resultCode) {
                    RESULT_OK -> Toast.makeText(this, "GPS Is Turned On", Toast.LENGTH_SHORT).show()
                    RESULT_CANCELED -> {
                    }
                }
            }
        }

--- WeatherApp/app/src/main/java/com/example/myapplication/View/Ui/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/myapplication/View/Ui/MainActivity.kt
@@ -25,81 +25,87 @@ import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(),MyInterface.DataView,MyInterface{
+class MainActivity : AppCompatActivity(), MyInterface.DataView, MyInterface {
 
-    private var presenter:Presenter?=null
-    private var selectedData:String?=null
-    private var list:ArrayList<String>?=null
-    private var tvTemp2:TextView?=null
-    private var tvTempmin2:TextView?=null
-    private var tvTempmax2:TextView?=null
-    private var dialog:AlertDialog?=null
-    private var autoCompleteTextView:AutoCompleteTextView?=null
+    private var presenter: Presenter? = null
+    private var selectedData: String? = null
+    private var list: ArrayList<String>? = null
+    private var tvTemp2: TextView? = null
+    private var tvTempmin2: TextView? = null
+    private var tvTempmax2: TextView? = null
+    private var dialog: AlertDialog? = null
+    private var autoCompleteTextView: AutoCompleteTextView? = null
     private var locationManager: LocationManager? = null
     private val REQUEST_CHECK_SETTINGS = 12 //any value i want
     private val REQUEST_CODE = 1
     private val permission = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        presenter= Presenter(this)
+        presenter = Presenter(this)
     }
-    override fun onResume()
-    {
+
+    override fun onResume() {
         super.onResume()
         getLocationOnlyOnce()
     }
-    override fun viewInit(value: ArrayList<String>)
-    {
-        tvTemp2=findViewById(R.id.tv_Temp2)
-        tvTempmin2=findViewById(R.id.tv_tempMin2)
-        tvTempmax2=findViewById(R.id.tv_tempMax2)
-        autoCompleteTextView=findViewById(R.id.attv)
-        val myAdapter=ArrayAdapter(this, android.R.layout.simple_list_item_1, value)
+
+    override fun viewInit(value: ArrayList<String>) {
+        tvTemp2 = findViewById(R.id.tv_Temp2)
+        tvTempmin2 = findViewById(R.id.tv_tempMin2)
+        tvTempmax2 = findViewById(R.id.tv_tempMax2)
+        autoCompleteTextView = findViewById(R.id.attv)
+        val myAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, value)
         attv.setAdapter(myAdapter)
-        list=value
+        list = value
         autoCompleteTextView()
     }
-    override fun updateViews(temp: String, temp1: String, temp2: String)
-    {
-        tvTemp2?.text="$temp °C"
-        tvTempmin2?.text="$temp1 °C"
-        tvTempmax2?.text="$temp2 °C"
+
+    override fun updateViews(temp: String, temp1: String, temp2: String) {
+        tvTemp2?.text = "$temp °C"
+        tvTempmin2?.text = "$temp1 °C"
+        tvTempmax2?.text = "$temp2 °C"
     }
-    override fun autoCompleteTextView()
-    {
-        attv.setOnItemClickListener{ parent, view, position, id ->
+
+    override fun autoCompleteTextView() {
+        attv.setOnItemClickListener { parent, view, position, id ->
             selectedData = parent.getItemAtPosition(position) as String
             presenter?.getData(selectedData!!)
         }
     }
-    override fun giveLocationDataToPresenter(latitude: String, longitude: String)
-    {
+
+    override fun giveLocationDataToPresenter(latitude: String, longitude: String) {
         presenter?.takeLocationDataFromView(latitude, longitude)
     }
-    override fun updateViewsByLocation(temp: String, temp1: String, temp2: String, city: String)
-    {
-        tvTemp2?.text="$temp °C"
-        tvTempmin2?.text="$temp1 °C"
-        tvTempmax2?.text="$temp2 °C"
+
+    override fun updateViewsByLocation(temp: String, temp1: String, temp2: String, city: String) {
+        tvTemp2?.text = "$temp °C"
+        tvTempmin2?.text = "$temp1 °C"
+        tvTempmax2?.text = "$temp2 °C"
         autoCompleteTextView?.setText(city)
     }
-    override fun requestPermission()
-    {
+
+    override fun requestPermission() {
         //ActivityCompat.shouldShowRequestPermissionRationale is true when all the permissions is not granted
         //and they are not granted when ActivityCompat.checkSelfPermission is !=granted
-        if ((ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity, permission[0]) &&
-                    ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity, Manifest.permission.ACCESS_COARSE_LOCATION)))
-        {
+        if ((ActivityCompat.shouldShowRequestPermissionRationale(
+                this@MainActivity,
+                permission[0]
+            ) &&
+                    ActivityCompat.shouldShowRequestPermissionRationale(
+                        this@MainActivity,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ))
+        ) {
             val alert = AlertDialog.Builder(this@MainActivity)
             alert.setTitle("Grant Those Permission")
-            alert.setMessage("The Following Permissions Needs Acceptance Please").setPositiveButton("OK")
-            { dialog, which ->
-                ActivityCompat.requestPermissions(this@MainActivity, permission, REQUEST_CODE)
-                dialog.dismiss()
-            }
+            alert.setMessage("The Following Permissions Needs Acceptance Please")
+                .setPositiveButton("OK")
+                { dialog, which ->
+                    ActivityCompat.requestPermissions(this@MainActivity, permission, REQUEST_CODE)
+                    dialog.dismiss()
+                }
             alert.setNegativeButton("Cancel")
             { dialog, which ->
                 Toast.makeText(this@MainActivity, "Permission Denied", Toast.LENGTH_SHORT).show()
@@ -117,6 +123,7 @@ class MainActivity : AppCompatActivity(),MyInterface.DataView,MyInterface{
             )
         }
     }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray)
     {
         var requestCode = requestCode
@@ -124,33 +131,19 @@ class MainActivity : AppCompatActivity(),MyInterface.DataView,MyInterface{
         when (1.also { requestCode = it }) {
             REQUEST_CODE -> {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        grantResults[0].toString() + "",
-                        Toast.LENGTH_SHORT
-                    ).show() //value of permission if granted
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Permission For Location Is Granted",
-                        Toast.LENGTH_SHORT
+                    Toast.makeText(this@MainActivity, grantResults[0].toString() + "", Toast.LENGTH_SHORT).show() //value of permission if granted
+                    Toast.makeText(this@MainActivity, "Permission For Location Is Granted", Toast.LENGTH_SHORT
                     ).show()
-                } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        grantResults[0].toString() + "",
-                        Toast.LENGTH_SHORT
-                    ).show() ///value of permission if denied
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Permission For Location Is Denied",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                }
+                else {
+                    Toast.makeText(this@MainActivity, grantResults[0].toString() + "", Toast.LENGTH_SHORT).show() ///value of permission if denied
+                    Toast.makeText(this@MainActivity, "Permission For Location Is Denied", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
-    override fun turnGpsOn()
-    {
+
+    override fun turnGpsOn() {
         val locationRequest = LocationRequest.create() //initialize the location Request parameter
         locationRequest.priority =
             LocationRequest.PRIORITY_HIGH_ACCURACY //to get the most accurate location
@@ -199,40 +192,39 @@ class MainActivity : AppCompatActivity(),MyInterface.DataView,MyInterface{
             }
         }
     }
-    override fun getLocationOnlyOnce()
-    {
+
+    override fun getLocationOnlyOnce() {
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
         val hasNetwork = locationManager!!.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-        if (hasNetwork)
-        {
-            if ((ContextCompat.checkSelfPermission(this@MainActivity, permission[0]) )//-1) //-1
-                == PackageManager.PERMISSION_GRANTED )//-3
+        if (hasNetwork) {
+            if ((ContextCompat.checkSelfPermission(this@MainActivity, permission[0]))//-1) //-1
+                == PackageManager.PERMISSION_GRANTED
+            )//-3
             {
                 locationManager!!.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f,
-                    object : LocationListener { override fun onLocationChanged(location: Location) {
-                            Toast.makeText(this@MainActivity, "Latitude:" + location.latitude, Toast.LENGTH_SHORT).show()
-                            Toast.makeText(this@MainActivity, "Longitude:" + location.longitude, Toast.LENGTH_SHORT).show()
+                    object : LocationListener {
+                        override fun onLocationChanged(location: Location) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Latitude:" + location.latitude,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Longitude:" + location.longitude,
+                                Toast.LENGTH_SHORT
+                            ).show()
                             Log.d("Success", "onLocationChanged: " + location.latitude)
                             Log.d("Success", "onLocationChanged: " + location.longitude)
-                            giveLocationDataToPresenter(location.latitude.toString(), location.longitude.toString())
+                            giveLocationDataToPresenter(
+                                location.latitude.toString(),
+                                location.longitude.toString()
+                            )
                             //after getting updates only once , the we remove updates by giving the method remove updates the same listener which we refer here
                             //as this since we are in the context of new LocationListener()
                             locationManager!!.removeUpdates(this)
                         }
                     })
-            }
-        }
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
-    {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode ==REQUEST_CHECK_SETTINGS) //if the result code related to the resultcode i'm using for GPS Enabling that i defined as
-        //    public static final int REQUEST_CHECK_SETTINGS=12; //any value i want
-        {
-            when (resultCode) {
-                RESULT_OK -> Toast.makeText(this, "GPS Is Turned On", Toast.LENGTH_SHORT).show()
-                RESULT_CANCELED -> {
-                }
             }
         }
     }
